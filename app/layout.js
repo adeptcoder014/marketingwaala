afb0e4a7c6ef15469482541cb7eeb9cc1fc95f76
Nischal Gupta <nischal@onecorp.co.in>
2024-06-11 17:05:42 +0530

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
  import { Inter } from "next/font/google";
 import "./globals.css";
-
+// import Image from "next/image";
+import Head from "next/head";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -10,7 +12,33 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+
+
+
+
     <html lang="en">
+      <Head>
+
+
+<title>MW</title>
+<meta name="description" content="MW" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="icon" href="/favicon.ico" />
+
+<Script
+  id="gtag-init"
+  type="text/javascript"
+  strategy="beforeInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5JSJSP7F')`}}>
+
+</Script>
+</Head>
+    
       <body className={inter.className}>{children}</body>
     </html>
   );
