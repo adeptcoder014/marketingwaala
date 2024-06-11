c4cdd3ea07f88d4a69ee49ceacc17018b4401e1d
Nischal Gupta <nischal@onecorp.co.in>
2024-06-11 17:03:50 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -41,17 +41,19 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
 
-      </Head>
-      <Script
-        id="gtag-init"
-        type="text/javascript"
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <Script
+          id="gtag-init"
+          type="text/javascript"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5JSJSP7F')`}} />
+})(window,document,'script','dataLayer','GTM-5JSJSP7F')`}}>
+
+        </Script>
+      </Head>
 
 
 
