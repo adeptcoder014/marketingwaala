53b816a66b41e1d60cc4b165977fbc7014db1617
Nischal Gupta <nischal@onecorp.co.in>
2024-06-11 19:23:09 +0530

    google tag added

--- app/layout.js
+++ app/layout.js
@@ -42,10 +42,11 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-WC63RJLL')`}} />
       <body className={inter.className}>
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WC63RJLL"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          height="0" width="0" style={{ "display": "none", "visibility": "hidden" }}></iframe></noscript>
         {children}
       </body>
     </html>
   );
 }
+
 
