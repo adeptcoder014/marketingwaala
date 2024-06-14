69bda09da942db93198e2c8e65af5c4ff81c58ba
Nischal Gupta <nischal@onecorp.co.in>
2024-06-14 10:39:01 +0530

    google ana;tyics added

--- app/layout.js
+++ app/layout.js
@@ -20,12 +20,14 @@ export default function RootLayout({ children }) {
       <Head>
 
 
+
         <title>MW</title>
         <meta name="description" content="The one stop solution to all the marketing probelms - Marketing Waala" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <link rel="icon" href="/favicon.ico" />
 
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-D7NTDWY9MK"></script>
 
 
       </Head>
@@ -40,6 +42,22 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WC63RJLL')`}} />
+
+
+      <Script
+        id="gtag-init"
+        type="text/javascript"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-D7NTDWY9MK');`
+        }} />
+      <script>
+
+      </script>
       <body className={inter.className}>
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WC63RJLL"
           height="0" width="0" style={{ "display": "none", "visibility": "hidden" }}></iframe></noscript>
